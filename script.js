24ddad0b7ee6bf9ffbd953cbdb5b7dc7bd429e63
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-05-30 12:17:30 +0545

    Add new feature in project

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let button = document.getElementById('button');
 let list = document.getElementById('list');
+let val = document.getElementById('val');
+let info = document.getElementById('data');
 button.onclick = () => {
-    let val = document.getElementById('data').value;
-    console.log(val);
-    list.innerHTML = val;
+    if (info.value === '') {
+        alert("Add your Task");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = info.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "*";
+        li.appendChild(span);
+    }
+    info.value = "";
 }
